ab8ee3a4d17199d35d8e70bd31de9be42f7f8a27
Paona Hijam <hpluwang@gmail.com>
2024-07-24 15:25:44 +0530

    project skeleton

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="student_performance",
+    version="0.0.1",
+    author="HPLuwang",
+    author_email="hpluwang@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
